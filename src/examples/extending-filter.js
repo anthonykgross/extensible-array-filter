87a7b5eb36c9c1cbc57dce4f9354d146f6a294ec
Anthony GROSS <anthony.gross@biggie.co>
2025-01-03 11:49:24 +0100

    Doc

---
+++ src/examples/extending-filter.js
@@ -0,0 +1,51 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var types_1 = require("@anthonykgross/extensible-array-filter/types");
+var filters_1 = require("@anthonykgross/extensible-array-filter/filters");
+var MyFilter = /** @class */ (function (_super) {
+    __extends(MyFilter, _super);
+    function MyFilter() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.assert = function (clause, item) {
+            var value = item[clause.field];
+            return typeof value === 'string' && new RegExp(clause.value, "gi").test(value);
+        };
+        return _this;
+    }
+    return MyFilter;
+}(filters_1.StringFilter));
+var filters = __assign(__assign({}, types_1.getDefaultFilters), { 'my-operator': MyFilter });
+var rows = [
+    { id: 1, name: 'Example 1' },
+    { id: 2, name: 'Example 2' },
+    { id: 3, name: 'Example 3' },
+];
+var result = rows.where([
+    { field: 'id', operator: 'my-operator', value: 1 },
+], filters);
---
+++ src/examples/extending-filter.ts
@@ -0,0 +1,30 @@
+import {} from "@anthonykgross/extensible-array-filter";
+import {Filters, Clause, getDefaultFilters} from "@anthonykgross/extensible-array-filter/types";
+import {StringFilter} from "@anthonykgross/extensible-array-filter/filters";
+
+class MyFilter extends StringFilter {
+    assert = (clause: Clause<any>, item: any) => {
+        let value = item[clause.field]
+        return typeof value === 'string' && new RegExp(clause.value, "gi").test(value)
+    }
+}
+
+let filters: Filters = {
+    ...getDefaultFilters,
+    'my-operator': MyFilter,
+}
+
+interface Row {
+    id: number;
+    name: string;
+}
+
+const rows: Row[] = [
+    { id: 1, name: 'Example 1'},
+    { id: 2, name: 'Example 2'},
+    { id: 3, name: 'Example 3'},
+];
+
+const result = rows.where([
+    { field: 'id', operator: 'my-operator', value: 1 },
+], filters);
